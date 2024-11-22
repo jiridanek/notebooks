8d728baa471ced82ba2af89f202063f7e298f946
Jiri Danek <jdanek@redhat.com>
2024-11-22 16:00:37 +0100

    fix test commands, hopefully

--- ci/cached-builds/has_tests.py
+++ ci/cached-builds/has_tests.py
@@ -4,11 +4,15 @@ import json
 import os
 import pathlib
 import typing
+import unittest
 
 import gha_pr_changed_files
+from tests.test_main import PROJECT_ROOT
 
-"""Determines whether we have deploy Makefile tests for this target or not"""
+"""Determines whether we have deploy Makefile tests for this target or not
 
+https://github.com/openshift/release/blob/master/ci-operator/config/opendatahub-io/notebooks/opendatahub-io-notebooks-main.yaml#L1485
+"""
 
 class Args(argparse.Namespace):
     """Type annotation to have autocompletion for args"""
@@ -20,18 +24,29 @@ def main() -> None:
     parser.add_argument("--target", type=str)
     args = typing.cast(Args, parser.parse_args())
 
-    has_tests = False
-    dirs = gha_pr_changed_files.analyze_build_directories(args.target)
-    for d in reversed(dirs):  # (!)
-        kustomization = pathlib.Path(d) / "kustomize/base/kustomization.yaml"
-        has_tests = has_tests or kustomization.is_file()
-        break  # TODO: check only the last directory (the top level layer) for now
+    has_tests = check_tests(args.target)
 
     if "GITHUB_ACTIONS" in os.environ:
         with open(os.environ["GITHUB_OUTPUT"], "at") as f:
             print(f"tests={json.dumps(has_tests)}", file=f)
 
     print(f"{has_tests=}")
+
+
+def check_tests(target: str) -> bool:
+    has_tests = False
+    dirs = gha_pr_changed_files.analyze_build_directories(target)
+    for d in reversed(dirs):  # (!)
+        kustomization = pathlib.Path(PROJECT_ROOT) / d / "kustomize/base/kustomization.yaml"
+        has_tests = has_tests or kustomization.is_file()
+        break  # TODO: check only the last directory (the top level layer) for now
+    return has_tests
+
+
+class TestCheckTests(unittest.TestCase):
+    def test_has_tests(self):
+        assert check_tests("base-c9s-python-3.11") is False
+        assert check_tests("jupyter-minimal-ubi9-python-3.9") is True
 
 
 if __name__ == "__main__":
