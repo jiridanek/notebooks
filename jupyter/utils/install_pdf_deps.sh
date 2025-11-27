a4dc611a55e02acc9307714d8a4341262078089b
Jiri Daněk <jdanek@redhat.com>
2025-11-27 18:15:01 +0100

    RHAIENG-2186: build(jupyter): install LaTeχ from c9s/rhel9 packages
    ```
      [5/7] STEP 8/12: RUN ./utils/install_pdf_deps.sh
      + PACKAGES=(texlive-adjustbox texlive-bibtex texlive-charter texlive-ec texlive-euro texlive-eurosym texlive-fpl texlive-jknapltx texlive-knuth-local texlive-lm-math texlive-marvosym texlive-mathpazo texlive-mflogo-font texlive-parskip texlive-plain texlive-pxfonts texlive-rsfs texlive-tcolorbox texlive-times texlive-titling texlive-txfonts texlive-ulem texlive-upquote texlive-utopia texlive-wasy texlive-wasy-type1 texlive-wasysym texlive-xetex)
      + dnf install -y texlive-adjustbox texlive-bibtex texlive-charter texlive-ec texlive-euro texlive-eurosym texlive-fpl texlive-jknapltx texlive-knuth-local texlive-lm-math texlive-marvosym texlive-mathpazo texlive-mflogo-font texlive-parskip texlive-plain texlive-pxfonts texlive-rsfs texlive-tcolorbox texlive-times texlive-titling texlive-txfonts texlive-ulem texlive-upquote texlive-utopia texlive-wasy texlive-wasy-type1 texlive-wasysym texlive-xetex
      Updating Subscription Management repositories.
      Red Hat Enterprise Linux AI (3.0) for RHEL 9 x8 955 kB/s | 270 kB     00:00
      Red Hat Enterprise Linux 9 for x86_64 - AppStre 143 MB/s |  74 MB     00:00
      Red Hat CodeReady Linux Builder for RHEL 9 x86_  39 MB/s |  14 MB     00:00
      Red Hat Enterprise Linux 9 for x86_64 - BaseOS  173 MB/s |  89 MB     00:00
      Red Hat Universal Base Image 9 (RPMs) - BaseOS  7.4 MB/s | 531 kB     00:00
      Red Hat Universal Base Image 9 (RPMs) - AppStre  37 MB/s | 2.6 MB     00:00
      Red Hat Universal Base Image 9 (RPMs) - CodeRea 5.9 MB/s | 287 kB     00:00
      No match for argument: texlive-tcolorbox
      Error: Unable to find a match: texlive-tcolorbox
      subprocess exited with status 1
    ```

--- jupyter/utils/install_pdf_deps.sh
+++ jupyter/utils/install_pdf_deps.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies required for Notebooks PDF exports
 
-set -euxo pipefail
+set -Eeuxo pipefail
 
 # Mapping of `uname -m` values to equivalent GOARCH values
 declare -A UNAME_TO_GOARCH
@@ -23,21 +23,64 @@ if [[ "$(uname -m)" == "s390x" || "$(uname -m)" == "ppc64le" ]]; then
     exit 0
 fi
 
-# tex live installation
-echo "Installing TexLive to allow PDf export from Notebooks"
-curl -fL https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz
-zcat < install-tl-unx.tar.gz | tar xf -
-rm install-tl-unx.tar.gz
-pushd install-tl-2*
-perl ./install-tl --no-interaction --scheme=scheme-small --texdir=/usr/local/texlive
-popd
-rm -rf install-tl-2*
-mv /usr/local/texlive/bin/"$(uname -m)-linux" /usr/local/texlive/bin/linux
-pushd /usr/local/texlive/bin/linux
-./tlmgr install tcolorbox pdfcol adjustbox titling enumitem soul ucs collection-fontsrecommended
-popd
+# https://github.com/rh-aiservices-bu/workbench-images/blob/main/snippets/ides/1-jupyter/os/os-packages.txt
+PACKAGES=(
+texlive-adjustbox
+texlive-bibtex
+texlive-charter
+texlive-ec
+texlive-euro
+texlive-eurosym
+texlive-fpl
+texlive-jknapltx
+texlive-knuth-local
+texlive-lm-math
+texlive-marvosym
+texlive-mathpazo
+texlive-mflogo-font
+texlive-parskip
+texlive-plain
+texlive-pxfonts
+texlive-rsfs
+# available in epel but not in rhel9
+#texlive-tcolorbox
+texlive-times
+texlive-titling
+texlive-txfonts
+texlive-ulem
+texlive-upquote
+texlive-utopia
+texlive-wasy
+texlive-wasy-type1
+texlive-wasysym
+texlive-xetex
+# https://bugzilla.redhat.com/show_bug.cgi?id=2284201
+#texlive-listingsutf8
+#texlive-pdfcol
+#texlive-tikzfill
+)
+
+dnf install -y "${PACKAGES[@]}"
+dnf clean all
+
+pdflatex --version
+
+# install texlive-tcolorbox by other means
+
+# /usr/share/texlive/texmf-local/
+TEXMFLOCAL=$(kpsewhich -var-value TEXMFLOCAL)
+
+# https://tex.stackexchange.com/questions/24636/where-can-i-find-which-packages-are-available-as-tds-zip
+# https://www.ctan.org/tex-archive/install/macros/latex/contrib
+wget --progress=dot:giga -O /tmp/tcolorbox.zip https://mirrors.ctan.org/install/macros/latex/contrib/tcolorbox.tds.zip
+unzip -q -o -d "${TEXMFLOCAL}" /tmp/tcolorbox.zip
+rm /tmp/tcolorbox.zip
+texhash "${TEXMFLOCAL}"
+
+kpsewhich tcolorbox.sty
 
 # pandoc installation
+# https://github.com/jgm/pandoc/releases/3.7.0.2
 curl -fL "https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-${ARCH}.tar.gz"  -o /tmp/pandoc.tar.gz
 mkdir -p /usr/local/pandoc
 tar xvzf /tmp/pandoc.tar.gz --strip-components 1 -C /usr/local/pandoc/
